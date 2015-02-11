9220a0a92ec2c3bbb01ccb7eb9960a86cff89d6a
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-11 15:40:56 +0900

    codechef

--- codechef/2015/02/STFM.java
+++ codechef/2015/02/STFM.java
@@ -3,41 +3,32 @@ import java.util.*;
 import java.io.*;
 import java.math.BigInteger;
 public class STFM {
-    static long[] f;
-    static int m;
+    static long m;
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        m = sc.nextInt();
-        f = new long[m];
-        f[0] = 0;
-        long fac = 1;
+        m = sc.nextLong();
+        int[] g = new int[(int) m];
+        int fac = 1;
         for (int i = 1; i < m; i++) {
-            fac = (fac * i) % m;
-            f[i] = (f[i - 1] + (i + 1) * i / 2 + fac * i) % m;
+            fac = (int) (((long) fac * i) % m);
+            g[i] = ((long) g[i - 1] + i * fac) % m;
+            //f[i] = (f[i - 1] + (i + 1) * i / 2 + fac * i) % m;
             // System.out.println("f " + i + ": " + f[i]);
         }
-        long ans = 0;
+
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            ans = (ans + f(sc.nextLong())) % m;
+            long p = sc.nextLong();
+            long a = p;
+            long b = p + 1;
+            if (a % 2 == 0) a /= 2;
+            else b /= 2;
+            int f = (a * b / 2) % m;
+            f = ((long) f * p) % m;
+            f = ((long)f + g[(int) Math.max(p, m - 1)]) % m;
+            ans = ((long) ans + f) % m;
         }
         System.out.println(ans);
-    }
-    public static int f(long p) {
-        if (p < m) return (int) f[(int) p];
-        long k = p - m;
-        BigInteger a = new BigInteger("" + m).multiply(new BigInteger("" + 2))
-            .add(new BigInteger("" + k))
-            .multiply(new BigInteger("" + k).add(BigInteger.ONE)).divide(new BigInteger("" + 2))
-            .add(ssq(k + m)).subtract(ssq(m - 1)).divide(new BigInteger("" + 2))
-            .add(new BigInteger("" + f[m - 1]))
-            .mod(new BigInteger("" + m));
-        return a.intValue();
-    }
-    public static BigInteger ssq(long p) {
-        BigInteger a = new BigInteger("" + p);
-        BigInteger b = a.add(BigInteger.ONE);
-        BigInteger c = a.multiply(new BigInteger("" + 2)).multiply(BigInteger.ONE);
-        return a.multiply(b).multiply(c);
     }
 }

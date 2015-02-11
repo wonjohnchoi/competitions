fc441aeceb98d6b758abbb45828f921155707a42
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-11 22:09:34 +0900

    Added java template

---
+++ google-code-jam/templates/A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+// TIME_USED:
+public class A {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+
+            // System.out.printf("Case #%d: %?", tc, ?);
+        }
+    }
+}

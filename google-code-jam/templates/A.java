7a45e71927c941255e4ce8385dd0e08a859f0e25
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-13 22:32:59 +0900

    Added debug func to templates/A.java

--- google-code-jam/templates/A.java
+++ google-code-jam/templates/A.java
@@ -2,6 +2,38 @@ import java.util.*;
 import java.io.*;
 // TIME_USED:
 public class A {
+    public static boolean DEBUG = true;
+    public static void d(Object o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+    public static void d(int o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+    public static void d(long o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+    public static void d(double o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+    public static void d(float o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+    public static void d(boolean o) {
+        if (DEBUG) {
+            System.out.println(o);
+        }
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();

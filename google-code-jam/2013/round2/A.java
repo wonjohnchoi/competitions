46edd09ef1a403502597c322cece67adbbe40845
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-15 22:58:38 +0900

    Finished A and improved submit.sh script

--- google-code-jam/2013/round2/A.java
+++ google-code-jam/2013/round2/A.java
@@ -33,22 +33,25 @@ public class A {
             System.out.println(o);
         }
     }
-
-    public static class Flow implements Comparable<Flow> {
-        long from, to, amount;
-        public Flow(long from, long to, long amount) {
-            this.from = from;
-            this.to = to;
+    public static class Event implements Comparable<Event> {
+        long stationIdx, amount;
+        boolean isEnter;
+        public Event(long stationIdx, long amount, boolean isEnter) {
+            this.stationIdx = stationIdx;
             this.amount = amount;
+            this.isEnter = isEnter;
         }
         @Override
-            public int compareTo(Flow f) {
-            if (from - f.from > 0) return 1;
-            if (from == f.from) return 0;
-            return -1;
+            public int compareTo(Event f) {
+            long diff = stationIdx - f.stationIdx;
+            if (diff > 0) return 1;
+            if (diff < 0) return -1;
+            if (isEnter == f.isEnter) return 0;
+            if (isEnter && !f.isEnter) return -1;
+            return 1;
         }
         public String toString() {
-            return String.format("from: %d, to: %d, amount: %d", from, to, amount);
+            return String.format("stationIdx: %d, amount: %d, isEnter: %b", stationIdx, amount, isEnter);
         }
     }
     public static void main(String args[]) {
@@ -58,47 +61,47 @@ public class A {
             long N, M;
             N = sc.nextLong();
             M = sc.nextLong();
-            ArrayList<Flow> flows = new ArrayList<Flow>();
-            for (int m = 0; m < M; m++) { // <= 1000
-                flows.add(new Flow(sc.nextLong(), sc.nextLong(), sc.nextLong()));
-            }
-            Collections.sort(flows);
-            long ans = 0;
+            ArrayList<Event> es = new ArrayList<Event>();
+            long cost = 0;
             long MOD = 1000002013;
-            while (!flows.isEmpty()) {
-                Flow flow = flows.remove(0);
-                d("size: " + flows.size());
-                d("removed: " + flow);
-                int j = 0;
-                while (j < flows.size() && flow.amount != 0) {
-                    Flow flow2 = flows.get(j);
-                    d("compare to: " + flow2);
-                    if (flow.from == flow2.from) {
-                        j++;
-                        continue;
-                    }
-                    if (flow.to >= flow2.from && flow.to < flow2.to) {
-                        d("Merging");
-                        if (flow2.amount <= flow.amount) {
-                            ans += (flow2.from - flow.from) * flow2.amount * (flow2.to - flow.to);
-                            ans %= MOD;
-                            flow.amount -= flow2.amount;
-                            flow2.from = flow.from;
-                            flows.add(0, flows.remove(j));
-                            j++;
-                        } else {
-                            ans += (flow2.from - flow.from) * flow.amount * (flow2.to - flow.to);
-                            ans %= MOD;
-                            flow2.amount -= flow.amount;
-                            flow.to = flow2.to;
-                            j = 0;
+            for (int m = 0; m < M; m++) { // <= 1000
+                long from = sc.nextLong();
+                long to = sc.nextLong();
+                long amount = sc.nextLong();
+                es.add(new Event(from, amount, true));
+                es.add(new Event(to, amount, false));
+                long i = to - from;
+                long costP = ((2 * N + 1 - i) * i / 2) % MOD;
+                costP = (costP * amount) % MOD;
+                cost = (cost + costP) % MOD;
+            }
+            Collections.sort(es);
+            d("events: " + es);
+            d("cost: " + cost);
+            long reducedCost = 0;
+            ArrayList<Event> sim = new ArrayList<Event>();
+            while (!es.isEmpty()) {
+                Event e = es.remove(0);
+                if (e.isEnter) {
+                    sim.add(e);
+                } else {
+                    while (e.amount != 0) {
+                        Event last = sim.get(sim.size() - 1);
+                        long used = Math.min(e.amount, last.amount);
+                        e.amount -= used;
+                        last.amount -= used;
+                        long i = e.stationIdx - last.stationIdx;
+                        if (last.amount == 0) {
+                            sim.remove(sim.size() - 1);
                         }
-                    } else {
-                        j++;
+                        long costP = ((2 * N + 1 - i) * i / 2) % MOD;
+                        costP = (costP * used) % MOD;
+                        reducedCost = (reducedCost + costP) % MOD;
                     }
                 }
             }
-            System.out.printf("Case #%d: %d\n", tc, ans);
+            d("reduced: " + reducedCost);
+            System.out.printf("Case #%d: %d\n", tc, (cost - reducedCost + MOD) % MOD);
         }
     }
 }
--- google-code-jam/templates/submit.sh
+++ google-code-jam/templates/submit.sh
@@ -1,1 +1,1 @@
-javac $1.java && java $1 < $1$2.in > $1$2.out && sudo cp $1$2.out /var/www/
+javac $1.java && java $1 < $1$2.in > $1$2.out && sudo cp $1$2.out /var/www/ && less $1$2.out

fa2ece329aa658e78519a9a22c1d8d6505dde5b0
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-16 12:20:50 +0900

    2011 round1a B WIP

---
+++ google-code-jam/2011/round1a/B.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+// TIME_USED:
+public class B {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            int N, M;
+            N = sc.nextInt(); // # words in D <= 10000
+            M = sc.nextInt(); // # lists to consider <= 100
+            // D1, ..., DN sc.next()
+            // L1, ..., LM sc.next() each 26 letters long. each letter exactly once.
+            
+
+            // w1 w2 ... wM best word for each list            
+            // System.out.printf("Case #%d: %?\n", tc, ?);
+        }
+    }
+}

5475e3bd8eab289d3dc1ef8ab7185ba6f3a49587
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-16 16:35:58 +0900

    Solved A

--- google-code-jam/2014/round1a/A.java
+++ google-code-jam/2014/round1a/A.java
@@ -41,81 +41,49 @@ public class A {
             int N;
             N = sc.nextInt();
             L = sc.nextInt();
-            // 0 0 ... 0 0 (length L)
-            // N lines.
-            ArrayList<ArrayList<String>> from, to;
-            from = new ArrayList<ArrayList<String>>();
-            to = new ArrayList<ArrayList<String>>();
-            from.add(new ArrayList<String>());
-            to.add(new ArrayList<String>());
+            Long[][] vals = new Long[2][N];
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < N; i++) {
+                    vals[j][i] = Long.parseLong(sc.next(), 2);
+                }
+            }
+            int minFlip = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                long key = vals[0][0] ^ vals[1][i];
+                boolean fail = false;
+                for (int j = 0; j < N; j++) {
+                    if (j == 0) continue;
+                    long search = vals[0][j] ^ key;
+                    boolean found = false;
+                    for (int k = 0; k < N; k++) {
+                        if (search == vals[1][k]) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        fail = true;
+                        break;
+                    }
+                }
+                if (!fail) {
+                    int flip = 0;
+                    for (char c : Long.toBinaryString(key).toCharArray()) {
+                        if (c == '1') {
+                            flip++;
+                        }
+                    }
+                    minFlip = Math.min(flip, minFlip);
+                }
+            }
 
-            for (int i = 0; i < N; i++) {
-                from.get(0).add(sc.next());
-            }
-            for (int i = 0; i < N; i++) {
-                to.get(0).add(sc.next());
-            }
-            int ans = find(from, to, 0);
             String output;
-            if (ans == Integer.MAX_VALUE) {
+            if (minFlip == Integer.MAX_VALUE) {
                 output = "NOT POSSIBLE";
             } else {
-                output = "" + ans;
+                output = "" + minFlip;
             }
             System.out.printf("Case #%d: %s\n", tc, output);
         }
     }
-    public static int find(ArrayList<ArrayList<String>> from, ArrayList<ArrayList<String>> to, int i) {
-        int ans = Integer.MAX_VALUE;
-        if (i == L) return 0;
-        for (int k = 0; k < 2; k++) {
-            System.gc();
-            ArrayList<ArrayList<String>> newFrom = new ArrayList<ArrayList<String>>();
-            ArrayList<ArrayList<String>> newTo = new ArrayList<ArrayList<String>>();
-            boolean fail = false;
-            for (int j = 0; j < from.size(); j++) {
-                ArrayList<String> from0 = new ArrayList<String>();
-                ArrayList<String> from1 = new ArrayList<String>();
-                ArrayList<String> to0 = new ArrayList<String>();
-                ArrayList<String> to1 = new ArrayList<String>();
-                for (String s : from.get(j)) {
-                    if (s.charAt(i) == '0') from0.add(s);
-                    else from1.add(s);
-                }
-                for (String s : to.get(j)) {
-                    if (s.charAt(i) == '0') to0.add(s);
-                    else to1.add(s);
-                }
-                if (k == 0) {
-                    if (from0.size() == to0.size()) {
-                        newFrom.add(from0);
-                        newTo.add(to0);
-                        newFrom.add(from1);
-                        newTo.add(to1);
-                    } else {
-                        fail = true;
-                        break;
-                    }
-                } else {
-                    if (from0.size() == to1.size()) {
-                        newFrom.add(from0);
-                        newTo.add(to1);
-                        newFrom.add(from1);
-                        newTo.add(to0);
-                    } else {
-                        fail = true;
-                        break;
-                    }
-                }
-            }
-            if (!fail) {
-                int val = find(newFrom, newTo, i + 1);
-                if (val != Integer.MAX_VALUE) {
-                    ans = Math.min(ans, val + k);
-                }
-            }
-        }
-        return ans;
-    }
-
 }

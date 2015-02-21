f3ad4bb9fc710b5cf5f4a53cf93d81e93d739112
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-22 03:00:14 +0900

    Made make_template command

--- google-code-jam/templates/Template.java
+++ google-code-jam/templates/Template.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class A {
+public class Template {
     public static PrintStream out = System.out;
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);

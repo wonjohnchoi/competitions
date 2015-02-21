b538cf177be912ee09de21cdd844c030b42a7268
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-22 03:00:47 +0900

    Ready to solve 2011 round1b

---
+++ google-code-jam/2011/round1b/A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class A {
+    public static PrintStream out = System.out;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+
+            // out.printf("Case #%d: %s\n", tc, ans);
+        }
+    }
+}

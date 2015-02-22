f8a5bbdd9535c4a982d62802f6e6b659ef694dc7
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-22 22:26:24 +0900

    Fixed owner permission of template script

--- google-code-jam/templates/make_template.sh
+++ google-code-jam/templates/make_template.sh
@@ -1,5 +1,6 @@
 cp ../../templates/Template.java $1.java &&
 sed 's/class Template/class '$1'/' $1.java > tmp.java &&
 mv tmp.java $1.java &&
+sudo chown wonjohnchoi: $1.java &&
 sudo chmod 664 $1.java
 

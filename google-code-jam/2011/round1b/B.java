d60e784468223a6ec9f8938fdc97335e0999e285
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-23 00:03:52 +0900

    Fixed B

--- google-code-jam/2011/round1b/B.java
+++ google-code-jam/2011/round1b/B.java
@@ -1,71 +1,46 @@
 import java.util.*;
 import java.io.*;
 public class B {
-    public static class Group {
-        double start, end, time;
-        int P, V;
-        public Group(int P, int V) {
-            this.P = P;
-            this.V = V;
-            time = (V - 1) / 2.0 * D;
-            start = P - time;
-            end = P + time;
-        }
-        public void move(double a) {
-            if (a == 0) return;
-            double used = Math.min(Math.abs(a), time);
-            time -= used;
-            start += used * dir(a);
-            end += used * dir(a);
-        }
-        public static double dir(double a) {
-            return a / Math.abs(a);
-        }
-        public String toString() {
-            return "(" + start + " " + end + ")";
-        }
-    }
     public static PrintStream out = System.out;
-    public static double D;
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
-            int C;
-            C = sc.nextInt();
-            D = sc.nextDouble();
-            Group[] gs = new Group[C];
-            double totTime = 0;
-            double maxTime = -1;
+            int C = sc.nextInt();
+            double D = sc.nextDouble();
+            double t = 0.0;
+            double b = -1;
             for (int i = 0; i < C; i++) {
                 int P, V;
                 P = sc.nextInt();
                 V = sc.nextInt();
-                gs[i] = new Group(P, V);
-                maxTime = Math.max(maxTime, gs[i].time);
+                double ct = (V - 1) / 2.0 * D;
+                double e = P + ct;
+                double s = P - ct;
+                if (i != 0 && b > s) {
+                    double used;
+                    if (ct > t) {
+                        used = Math.min(ct - t, b - s);
+                        b -= used;
+                        t += used;
+                    } else if (ct < t) {
+                        used = Math.min(t - ct, b - s);
+                        s += used;
+                        e += used;
+                        ct += used;
+                    }
+                    if (b > s) {
+                        double need = (b - s) / 2.0;
+                        ct = t + need;
+                        s += need;
+                        e += need;
+                        b -= need;
+                    }
+                }
+                t = Math.max(t, ct);
+                b = e + D;
             }
-            totTime += maxTime;
-            for (int i = 0; i < C; i++) {
-                gs[i].time = maxTime - gs[i].time;
-            }
-            for (int i = 0; i < C; i++) {
-                spendTime(gs, 0);
-            }
-            for (int i = 1; i < gs.length; i++) {
-                double time = (gs[i - 1].end - gs[i].start + D) / 2;
-                if (time < 0) time = 0;
-                spendTime(gs, time);
-                totTime += time;
-            }
-            out.printf("Case #%d: %s\n", tc, totTime + "");
-        }
-    }
-    public static void spendTime(Group[] gs, double time) {
-        for (int i = 0; i < gs.length; i++) {
-            gs[i].time += time;
-            if (i == 0) gs[i].move(-time);
-            else gs[i].move(gs[i - 1].end - gs[i].start + D);
-            gs[i].time = 0;
+            out.printf("Case #%d: %f\n", tc, t);
         }
     }
 }

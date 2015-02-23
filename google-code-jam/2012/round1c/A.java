dd1ea32c9516e4d5321a8756742dd8166fa2b9c2
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-23 15:04:16 +0900

    Solved A

--- google-code-jam/2012/round1c/A.java
+++ google-code-jam/2012/round1c/A.java
@@ -2,91 +2,73 @@ import java.util.*;
 import java.io.*;
 public class A {
     public static PrintStream out = System.out;
-    public static int N;
+    public static class Node {
+        List<Node> from;
+        int cnt;
+        int idx;
+        public Node() {
+            cnt = 0;
+            from = new ArrayList<Node>();
+        }
+    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt();
-            List[] graph = new List[N];
+            Node[] nodes = new Node[N];
+            for (int i = 0; i < N; i++) {
+                nodes[i] = new Node();
+            }
             for (int i = 0; i < N; i++) {
                 int M = sc.nextInt();
-                graph[i] = new ArrayList();
                 for (int j = 0; j < M; j++) {
-                    graph[i].add(sc.nextInt() - 1);
+                    nodes[i].from.add(nodes[sc.nextInt() - 1]);
                 }
+                nodes[i].idx = i;
             }
             boolean yes = false;
-            for (int i = 0; i < N && !yes; i++) {
-                int[] tot = new int[N];
-                boolean[] visited = new boolean[N];
-                visited[i] = true;
-                tot[i] = 1;
-                for (int j = 0; j < N; j++) {
-                    if (tot[j] + (visited[j] ? 1 : 0) >= 2 && i != j) {
+            List<Node> sorted = tSort(nodes);
+            for (int i = 0; i < sorted.size(); i++) {
+                for (int j = 0; j < sorted.size(); j++) {
+                    sorted.get(j).cnt = 0;
+                }
+                sorted.get(i).cnt = 1;
+                for (int j = i + 1; j < sorted.size(); j++) {
+                    for (Node node : sorted.get(j).from) {
+                        sorted.get(j).cnt += node.cnt;
+                    }
+                    if (sorted.get(j).cnt >= 2) {
                         yes = true;
-                        break;
-                    } else if (tot[j] >= 1) {
-                        for (Object o : graph[j]) {
-                            int next = (Integer) o;
-                            tot[next] += tot[j];
-                        }
-                        tot[j] = 0;
-                        visited[j] = true;
                     }
                 }
             }
-            /* works for small
-            boolean[] visited = new boolean[N];
-            for (int i = 0; i < N && !yes; i++) {
-                for (int j = 0; j < N && !yes; j++) {
-                    tot = 0;
-                    visited[i] = true;
-                    yes(graph, i, j, visited);
-                    if (tot >= 2) {
-                        yes = true;
-                    }
-                    visited[i] = false;
-                }
-                }*/
             String ans = yes ? "Yes" : "No";
             out.printf("Case #%d: %s\n", tc, ans);
         }
     }
-    public static int yes1(List[] graph, int i, int j) {
-        // boolean[] visited = new boolean[N];
-        List<Integer> frontiers = new ArrayList<Integer>();
-        frontiers.add(i);
-        // visited[i] = true;
-        int tot = 0;
-        while (!frontiers.isEmpty()) {
-            int cur = frontiers.remove(0);
-            for (Object o : graph[cur]) {
-                int next = (Integer) o;
-                if (next == j) {
-                    tot++;
-                    if (tot >= 2) return 2;
+    public static List<Node> tSort(Node[] nodes2) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        for (Node node : nodes2) {
+            nodes.add(node);
+        }
+        List<Node> res = new ArrayList<Node>();
+        while (!nodes.isEmpty()) {
+            int i = 0;
+            while (i < nodes.size()) {
+                int cnt = 0;
+                for (Node node : nodes.get(i).from) {
+                    if (!res.contains(node)) {
+                        cnt++;
+                    }
+                }
+                if (cnt == 0) {
+                    res.add(nodes.remove(i));
                 } else {
-                    frontiers.add(next);
+                    i++;
                 }
             }
         }
-        return tot;
-    }
-    static int tot;
-    public static void yes(List[] graph, int i, int j, boolean[] visited) {
-        if (i == j) {
-            tot++;
-            return;
-        }
-        if (tot >= 2) return;
-        for (Object o : graph[i]) {
-            int next = (Integer) o;
-            if (!visited[next]) {
-                visited[next] = true;
-                yes(graph, next, j, visited);
-                visited[next] = false;
-            }
-        }
+        return res;
     }
 }

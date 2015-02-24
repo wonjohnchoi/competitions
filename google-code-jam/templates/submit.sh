59224bd41f0a3d4e166d9a8f31fb795530330468
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-24 10:27:46 +0900

    Improved template

--- google-code-jam/templates/submit.sh
+++ google-code-jam/templates/submit.sh
@@ -1,1 +1,2 @@
-javac $1.java && java $1 < $1$2.in > $1$2.out && sudo cp $1$2.out /var/www/ && less $1$2.out
+javac $1.java && java $1 < $1$2.in > $1$2.out && sudo cp $1$2.out /var/www/ &&
+sudo cp $1.java /var/www/ && less $1$2.out

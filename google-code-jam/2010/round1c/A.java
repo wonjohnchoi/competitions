50ea808af4c47c3b0763e1e9c1cc03763d4c3623
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-27 21:54:30 +0900

    Ready to start 2010 round1c for real

---
+++ google-code-jam/2010/round1c/A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class A {
+    public static PrintStream out = System.out;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int TC = sc.nextInt();
+        for (int tc = 1; tc <= TC; tc++) {
+            String ans = "";
+            out.printf("Case #%d: %s\n", tc, ans);
+        }
+    }
+}

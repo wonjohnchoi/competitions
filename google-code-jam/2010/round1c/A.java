7ed74876ad6f6124ff229ff8240a64ba0b23b74a
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-02-27 22:02:10 +0900

    Solved A

--- google-code-jam/2010/round1c/A.java
+++ google-code-jam/2010/round1c/A.java
@@ -6,7 +6,24 @@ public class A {
         Scanner sc = new Scanner(System.in);
         int TC = sc.nextInt();
         for (int tc = 1; tc <= TC; tc++) {
-            String ans = "";
+            int N = sc.nextInt();
+            int[] As = new int[N];
+            int[] Bs = new int[N];
+            for (int i = 0; i < N; i++) {
+                As[i] = sc.nextInt();
+                Bs[i] = sc.nextInt();
+            }
+            int cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    int Ad = As[i] - As[j];
+                    int Bd = Bs[i] - Bs[j];
+                    if ((Ad < 0 && Bd > 0) || (Ad > 0 && Bd < 0)) {
+                        cnt++;
+                    }
+                }
+            }
+            String ans = "" + cnt;
             out.printf("Case #%d: %s\n", tc, ans);
         }
     }

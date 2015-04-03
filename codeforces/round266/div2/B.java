3505add80eac1c43a18910f8b62c542850b8c009
Wonjohn Choi <wonjohn.choi@gmail.com>
2015-04-03 09:09:24 +0900

    working on B round266 div2

--- codeforces/round266/div2/B.java
+++ codeforces/round266/div2/B.java
@@ -1,10 +1,33 @@
 import java.util.*;
 import java.io.*;
-public class Template {
+public class B {
     public static PrintWriter out = new PrintWriter(System.out);
     public static InputReader in = new InputReader(System.in);
     public static void main(String args[]) {
-
+        long N, A, B;
+        N = in.nextLong();
+        A = in.nextLong();
+        B = in.nextLong();
+        long need = 6L * N;
+        outer : while (true) {
+            if (need <= A * B) {
+                out.println(A * B);
+                out.println(A + " " + B);
+                break;
+            }
+            for (long i = 1; i * i <= need ; i++) {
+                long j = need / i;
+                boolean a = i >= A && j >= B;
+                boolean b = i >= B && j >= A;
+                if (need % i == 0 && (a || b)) {
+                    out.println(need);
+                    if (a) out.println(i + " " + j);
+                    else out.println(j + " " + i);
+                    break outer;
+                }
+            }
+            need++;
+        }
         out.close();
     }
 }
@@ -31,8 +54,4 @@ class InputReader {
     public long nextLong() {
 	return Long.parseLong(next());
     }
-    /*
-    public int nextInt() {
-	return Integer.parseInt(next());
-        }*/
 }
